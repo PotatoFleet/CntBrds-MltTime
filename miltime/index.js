3a7239b335fca99c1c94cdcae96b4e0e89c01203
PotatoFleet <supsrasri@gmail.com>
2022-10-09 21:17:38 +0530

    added comments

--- miltime/index.js
+++ miltime/index.js
@@ -1,16 +1,15 @@
 const miltime = (time) => {
+  // 12 -> 00 simplifies things
   time = time.replace("12", "00");
+  // Get data from time
   const hour = parseInt(time[0] + time[1]);
   const minute = parseInt(time[3] + time[4]);
   const second = parseInt(time[6] + time[7]);
   const type = time[8] + time[9];
+  // Compute total seconds
   const totalSeconds =
-    second +
-    minute * 60 +
-    hour * 3600 +
-    (type === "PM" ? 12 * 3600 : 0) +
-    45 * 61;
-  console.log(totalSeconds);
+    second + minute * 60 + hour * 3600 + (type === "PM" ? 43200 : 0) + 2745;
+  // Compute final values
   const hours = Math.floor(totalSeconds / 3600)
     .toString()
     .padStart(2, "0");
@@ -22,6 +21,3 @@ const miltime = (time) => {
     .padStart(2, "0");
   return `${hours}:${minutes}:${seconds}`;
 };
-
-const res = miltime("12:01:00PM");
-console.log(res);
